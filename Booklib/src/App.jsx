1fca512cb795f501e8a1a4319c72575619d722b3
Sandhya154321 <sandhyamote44@gmail.com>
2025-01-01 16:29:56 +0530

    added bootstrap component

--- Booklib/src/App.jsx
+++ Booklib/src/App.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
 import Main_sec from './Components/Main_sec'
+import Cards from './Components/Cards'
 
 const App = () => {
   return (
 <>
 <Navbar />
 <Main_sec />
+<Cards />
 <Footer />
-
 </>  )
 }
 
